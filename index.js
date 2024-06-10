7cc6290bd12945aadd9409fe3726582936d55117
Yashasvi Kushwah <135824083+YashasviKushwah06@users.noreply.github.com>
2024-06-10 15:44:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let weather = {
+    APIkey: "07ed9e79b7d0dac2185a41c64863bb34",
+    fetchWeather: function (city)  {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city 
+            + "&units=metric&APPID=" 
+            + this.APIkey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://picsum.photos/1600/900')";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+}
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+
+weather.fetchWeather("New Delhi");
